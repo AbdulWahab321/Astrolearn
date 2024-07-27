d149038f96f54bb2303ea55287c2bd00104d362f
AbdulWahab321 <jr.abdulwahab@gmail.com>
2024-07-27 23:55:28 +0530

    fixes

--- lib/db.py
+++ lib/db.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import firebase_admin, os, json
 from firebase_admin import credentials, firestore
 from datetime import datetime
@@ -42,48 +41,3 @@ def get_homeworks():
         for hw in hw_list:
             if hasattr(hw["due"],"isoformat"):hw["due"] = hw["due"].isoformat()  # Convert datetime to ISO string for JSON serialization
     return homeworks_data
-=======
-import firebase_admin, os, json
-from firebase_admin import credentials, firestore
-from datetime import datetime
-
-DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data')
-ASTROLEARNJSON = "/etc/secrets/astrolearn_service_account.json"
-if not os.path.exists(ASTROLEARNJSON):
-    ASTROLEARNJSON = os.path.join(DATA_DIR, "astrolearn.json")
-
-cred = credentials.Certificate(ASTROLEARNJSON)
-app = firebase_admin.initialize_app(cred)
-db = firestore.client(app)
-schoolstuffs = db.collection('SchoolStuffs')
-
-homeworks = schoolstuffs.document("homeworks")
-exams = schoolstuffs.document("exams")
-assigned_to_study = schoolstuffs.document("assignedtostudy")
-
-def db_update_homework(subject, chapter, title, description, due):
-    subject_homeworks = homeworks.get().to_dict().get(subject, [])
-    subject_homeworks.append({
-        "chapter": chapter,
-        "homework_title": title,
-        "description": description,
-        "due": due  # Firestore will automatically convert this to a Firestore timestamp
-    })
-    homeworks.update({
-        subject: subject_homeworks
-    })
-
-def remove_homework(subject, title):
-    subject_homeworks = homeworks.get().to_dict().get(subject, [])
-    updated_homeworks = [hw for hw in subject_homeworks if hw["homework_title"] != title]
-    homeworks.update({
-        subject: updated_homeworks
-    })
-
-def get_homeworks():
-    homeworks_data = homeworks.get().to_dict()
-    for subject, hw_list in homeworks_data.items():
-        for hw in hw_list:
-            if hasattr(hw["due"],"isoformat"):hw["due"] = hw["due"].isoformat()  # Convert datetime to ISO string for JSON serialization
-    return homeworks_data
->>>>>>> 445d473cad7454cad9dbdc82ea9a0224b7c991ae
